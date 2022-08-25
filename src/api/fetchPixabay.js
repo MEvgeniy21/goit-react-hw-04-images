ac4c9a4ce060c578d60a25374f6291522db77083
Evgeniy <95440463+MEvgeniy21@users.noreply.github.com>
2022-08-26 02:34:16 +0300

    commit

--- src/api/fetchPixabay.js
+++ src/api/fetchPixabay.js
@@ -2,10 +2,10 @@ import axios from 'axios';
 
 axios.defaults.baseURL = 'https://pixabay.com/api/';
 
-export async function fetchImage({ q, page, per_page }) {
+export async function fetchImage({ search, page, per_page }) {
   const searchParams = new URLSearchParams({
     key: '28350723-a2da361fc29768379678747a9',
-    q,
+    q: search,
     page,
     per_page,
     image_type: 'photo',
